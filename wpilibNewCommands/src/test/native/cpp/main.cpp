66954b7b13ef3131d09eda2404ae68f8f2d17be7
Isaac Turner <spacey-sooty@proton.me>
2024-05-12 01:19:45 +0800

    convert newCommands main file

--- wpilibNewCommands/src/test/native/cpp/main.cpp
+++ wpilibNewCommands/src/test/native/cpp/main.cpp
@@ -2,12 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <gtest/gtest.h>
+#include <catch2/catch_session.hpp>
 #include <hal/HALBase.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
   HAL_Initialize(500, 0);
-  ::testing::InitGoogleTest(&argc, argv);
-  int ret = RUN_ALL_TESTS();
-  return ret;
+
+  int result = Catch::Session().run(argc, argv);
+
+  return result;
 }
