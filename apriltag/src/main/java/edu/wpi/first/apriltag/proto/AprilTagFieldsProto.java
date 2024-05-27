c14906b17133b521e77098bc34a70303dc0c4030
Isaac Turner <spacey-sooty@proton.me>
2024-05-27 21:34:49 +0800

    failing

--- apriltag/src/main/java/edu/wpi/first/apriltag/proto/AprilTagFieldsProto.java
+++ apriltag/src/main/java/edu/wpi/first/apriltag/proto/AprilTagFieldsProto.java
@@ -1,27 +1,39 @@
-//package edu.wpi.first.apriltag.proto;
-//
-//import edu.wpi.first.apriltag.AprilTagFields;
-//import edu.wpi.first.apriltag.proto.AprilTags.ProtobufAprilTagFields;
-//import edu.wpi.first.util.protobuf.Protobuf;
-//import us.hebi.quickbuf.Descriptors.Descriptor;
-//
-//public class AprilTagFieldsProto implements Protobuf<AprilTagFields, ProtobufAprilTagFields> {
-//    @Override
-//    public Class<AprilTagFields> getTypeClass() {
-//        return ProtobufAprilTagFields.class;
-//    }
-//
-//    @Override
-//    public Descriptor getDescriptor() {
-//        return ProtobufAprilTagFields.getDescriptor();
-//    }
-//
-//  @Override
-//  public ProtobufAprilTagFields createMessage() {
-//    return ProtobufAprilTagFields.newInstance();
-//  }
-//
-//  @Override
-//  public void pack(ProtobufAprilTagFields msg, AprilTagFields value) {
-//  }
-//}
+package edu.wpi.first.apriltag.proto;
+
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.proto.AprilTags.ProtobufAprilTagFields;
+import edu.wpi.first.util.protobuf.Protobuf;
+
+public class AprilTagFieldsProto implements Protobuf<AprilTagFields, ProtobufAprilTagFields> {
+  @Override
+  public Class<AprilTagFields> getTypeClass() {
+    return AprilTagFields.class;
+  }
+
+  @Override
+  public ProtobufAprilTagFields createMessage() {
+    return ProtobufAprilTagFields.kDefaultField;
+  }
+
+  @Override
+  public AprilTagFields unpack(ProtobufAprilTagFields msg) {
+    switch (msg.getNumber()) {
+      case 0:
+        return AprilTagFields.k2022RapidReact;
+        break;
+      case 1:
+        return AprilTagFields.k2023ChargedUp;
+        break;
+      case 2:
+        return AprilTagFields.k2024Crescendo;
+        break;
+      default:
+        return AprilTagFields.kDefaultField;
+        break;
+    }
+  }
+
+  @Override
+  public void pack(ProtobufAprilTagFields msg, AprilTagFields value) {
+  }
+}
